7fc1ad4dfbc84f2366a5ac3f0ab93db2037dfd7c
xdock7 <76634793+xdock7@users.noreply.github.com>
2024-05-05 21:59:44 +0800

    Proper version

--- hack_FBI.py
+++ hack_FBI.py
@@ -1,41 +1,51 @@
 import time
+import random
 
-# Define available commands
-commands = {
-    "help": "Check available commands",
-    "meme": "Get a meme",
-    "hack": "Hack FBI",
-    "exit": "Quit program"}
+# available commands
+commands = {"help":"Show available commands",
+            "meme":"Mysterious",
+            "hack":"Hack somebody",
+            "exit":"Quit console window"
+            }
 
-def display_commands():
-    """Display available commands"""
-    for key, value in commands.items():
+# list of memes
+memes = (["https://shorturl.at/berB9", "https://rb.gy/erd9xz", "https://rb.gy/hv64c3", "https://rb.gy/i0ik1q"])
+ranmemes = random.choice(memes)
+
+# help
+def available_commands():
+    for key,value in commands.items():
         print(f"{key} - {value}")
 
-def hack_fbi():
-    """Simulate hacking FBI"""
+# fake hacking part, force uppercase for victims
+def hack():
+    victims = input("Who do you want to hack? : ").upper()
+    print("Command accepted, start hacking...")
+    time.sleep(0.4)
     for n in range(100):
-        print(f"Hacking FBI {n}%")
-        time.sleep(0.3)
-    print("FBI hacked successfully!")
+        print(f"Hacking {victims} {n+1}%")
+        time.sleep(0.2)
+    print(f"{victims} hacked successfully!")
 
+# define of the list of memes
+def memes():
+    print(ranmemes)
+
+# Register user input
 def main():
-    while True:
-        i = input('Command (type "help" to check available commands): ').lower()
-        if i == "help":
-            display_commands()
-            #time.sleep(5)
-        elif i == "hack":
-            hack_fbi()
-            input()
-        elif i == "meme":
-            print("[1](https://www.youtube.com/watch?v=BbeeuzU5Qc8)")
-            time.sleep(5)
-        elif i == "exit":
-            exit()
-        else:
-            print("Invalid command. Please try again.")
-            time.sleep(3)
+        while True:
+            i = input("Command: ").lower()
+            if i == 'help':
+                available_commands()
+            elif i == 'meme':
+                memes()
+            elif i == 'hack':
+                hack()
+            elif i == 'exit':
+                exit()
+            else:
+                print("An error occured, sorry :(")
+
 
 if __name__ == "__main__":
     main()
